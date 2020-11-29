7932344a107343c1a9329461490cb73c9b26ee9c
Shrey2006 <shrey007yadav@gmail.com>
2020-11-29 17:42:02 +0530

    change is done

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var Chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,10 +36,13 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230,180,80,PI/2);
+    bird = new Bird(100,100); 
+    Chain = new chain(bird.body,log6.body);
+    
 
-    bird = new Bird(100,100);
 
-}
+} 
 
 function draw(){
     background(backgroundImg);
@@ -58,6 +64,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
+    Chain.display();
+    
 
     bird.display();
     platform.display();
